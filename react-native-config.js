94761064a0446ace306ffc6880a39e1bf0d4ef44
Christopher Frydryck <cfrydryck@gmail.com>
2021-02-13 16:32:35 -0700

    added dependencies with successfuly compile for iOS and Android

---
+++ react-native-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    dependencies: {
+      'tipsi-stripe': {
+        platforms: {
+          android: null,
+          ios: null,
+        }
+      }
+    },
+      project: {
+          ios: {},
+          android: {},
+      },
+      assets: ['./assets/fonts'],
+      
+  };
+  
+  
