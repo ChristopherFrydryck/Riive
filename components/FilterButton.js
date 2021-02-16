2ebbe044c5f42b9496e1050b446dcf95799b3b75
Christopher Frydryck <cfrydryck@gmail.com>
2021-02-15 20:28:58 -0700

    Added new components and working on HomeScreen functions

---
+++ components/FilterButton.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import {StyleSheet, TouchableOpacity} from 'react-native'
+
+import Text from '../components/Txt'
+import Colors from '../constants/Colors'
+
+export default class FilterButton extends Component{
+    render(){
+        return(
+            <TouchableOpacity onPress={() => this.setState(this.props.onPress)} disabled={this.props.disabled ? true : false} style={this.props.searchFilterOpen ? styles.filterButtonOpen : styles.filterButtonClosed}>
+                {/* {this.props.searchFilterOpen ?
+                <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end'}}>
+                    <Text style={{paddingRight: 8}}>Close Filters</Text>
+                    <Icon iconSize={24} iconName="arrow-up"/>                
+                </View>
+                
+                : */}
+                <Text numberOfLines={2} style={{fontSize: 13, lineHeight: 16}}>{`${this.props.daySearched.dayName}  \n${this.props.timeSearched[0].labelFormatted} to ${this.props.timeSearched[1].labelFormatted}`}</Text>
+                {/* } */}
+                
+            </TouchableOpacity>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    filterButtonClosed:{
+        flex: 2,
+        alignItems: 'flex-end',
+        borderLeftWidth: 5, 
+        borderLeftColor: Colors.tango900, 
+        paddingLeft: 8, 
+        marginLeft: 8, 
+    },
+    filterButtonOpen:{
+        flex: 1,
+        paddingVertical: 8,
+    },
+})
---
+++ components/Image.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { View, StyleSheet, Image, Dimensions, ImageBackground, TouchableOpacity } from 'react-native';
+import backupImage from '../assets/img/LoadingImage.jpg'
+
+// Props:
+// source
+// backupSource = backup if image fails or is loading
+// underlayColor
+// backupSource
+// aspectRatio
+// resizeMode
+// enabled
+// onPress
+// accessibilityLabel
+
+class Img extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            isReady: false,
+        }
+    }
+
+    render(){
+        const style = [styles.img,  this.props.style || {}]
+        const allProps = Object.assign({}, this.props,{style:style}) 
+
+        return(
+            <TouchableOpacity
+
+                onPress={this.props.onPress}
+                disabled={this.props.onPress ? false : true}
+                {...allProps}
+            >
+                
+                <ImageBackground
+                    source={this.props.backupSource ? this.props.backupSource : backupImage}
+                    style={{width: this.props.width, height: this.props.height,}}
+                    imageStyle={{aspectRatio: this.props.aspectRatio, resizeMode: this.props.resizeMode,  borderRadius: this.props.borderRadius}}
+                    
+                    // {...allProps}
+                >
+                    <Image 
+                        
+                        underlayColor = {this.props.underlayColor}
+                        style={{aspectRatio: this.props.aspectRatio, resizeMode: this.props.resizeMode, width: this.props.width, height: this.props.height, borderRadius: this.props.borderRadius}}
+                        source={this.props.source}
+                        defaultSource = {this.props.backupSource}
+                        onLoad = {() => this.setState({imgReady: true})}
+                        // {...allProps}
+                    />
+                </ImageBackground>
+                
+            </TouchableOpacity>
+        )
+    }    
+}
+
+const styles = StyleSheet.create({
+    img: {
+        height: null,
+        width: null,
+        
+    }
+})
+
+export default Img
