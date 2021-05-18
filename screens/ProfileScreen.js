b206954b6d89e6df36533ae64fe9067f29716c5b
Christopher Frydryck <cfrydryck@gmail.com>
2021-05-17 23:17:22 -0600

    working on adding address field to edit profile modal

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -4,6 +4,7 @@ import LinearGradient from 'react-native-linear-gradient'
 import {NavigationActions} from 'react-navigation'
 import Input from '../components/Input'
 import Button from '../components/Button'
+import AddressTypes from '../constants/AddressTypes'
 
 
 
@@ -15,12 +16,14 @@ import ProfilePic from '../components/ProfilePic'
 import TopBar from '../components/TopBar'
 import Icon from '../components/Icon'
 import Circle from '../components/Circle'
+import Dropdown from '../components/Dropdown'
 import Text from '../components/Txt'
 import VehicleList from '../components/VehicleList'
 import PaymentList from '../components/PaymentList'
 import SpacesList from '../components/SpacesList'
 import ClickableChip from '../components/ClickableChip'
 import DialogInput from 'react-native-dialog-input'
+import GooglePlacesAutocomplete from 'react-native-google-places-autocomplete'
 // import Dialog from 'react-native-dialog'
 // import * as ImagePicker from 'expo-image-picker'
 // import * as Permissions from 'expo-permissions'
@@ -101,6 +104,22 @@ class Profile extends Component{
             selectedImageURI: "",
             verificationSnackbarVisible: false,
             menuVisible: false,
+
+
+
+            searchedAddress: false,
+            address: {
+              line1: this.props.UserStore.address.line1,
+              line2: this.props.UserStore.address.line2 ? this.props.UserStore.address.line2.split(" ")[1] : "",
+              line2Prefix: this.props.UserStore.address.line2 ? this.props.UserStore.address.line2.split(" ")[0] : "Apartment",
+              zipCode: this.props.UserStore.address.postal_code,
+              city: this.props.UserStore.address.city,
+              state: this.props.UserStore.address.state,
+              country: this.props.UserStore.address.country
+            },
+            addressError: "",
+
+
             
             isRefreshing: false,
         }
@@ -123,6 +142,15 @@ class Profile extends Component{
             StatusBar.setBarStyle('light-content', true);
             Platform.OS === 'android' && StatusBar.setBackgroundColor(Colors.tango900);
           });
+
+          
+
+        //   if(this.state.address.line1){
+            this.setLocation(`${this.state.address.line1}, ${this.state.address.city} ${this.state.address.state} ${this.state.address.zipCode}, ${this.state.address.country}`)
+        //   }else{
+        //    this.setLocation("")
+        //   }
+          
 
       
 
@@ -153,6 +181,121 @@ class Profile extends Component{
         // Unmount status bar info
         this._navListener.remove();
     }
+
+    addAddress = async () => {
+
+        const settings = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            FBID: auth().currentUser.uid,
+            stripeID: this.props.UserStore.stripeID,
+            stripeConnectID: this.props.UserStore.stripeConnectID,
+      
+            lineOne: this.state.address.line1,
+            lineTwo: this.state.address.line2 == "" ? null : `${this.state.address.line2Prefix} ${this.state.address.line2}`,
+            zipCode: this.state.address.zipCode,
+            city: this.state.address.city,
+            state: this.state.address.state,
+          })
+        }
+      
+        if(this.state.address.line1 != ""){
+          try{  
+            const fetchResponse = await fetch('https://us-central1-riive-parking.cloudfunctions.net/addCustomerAddress', settings)
+            const data = await fetchResponse;
+            if(data.status !== 200){
+              throw "Failure to link address."
+            }
+            this.props.UserStore.address = {
+              line1: this.state.address.line1,
+              line2: this.state.address.line2 == "" ? null : `${this.state.address.line2Prefix} ${this.state.address.line2}`,
+              postal_code: this.state.address.zipCode,
+              city: this.state.address.city,
+              state: this.state.address.state,
+              country: this.state.address.country
+            }
+            return data;
+          }catch(e){
+            throw e
+          }  
+        }else{
+          throw "Failure to save address."
+        }
+          
+      }
+
+
+      onSelectAddress = async(det) => {
+        // console.log(det.formatted_address)
+        // console.log(det.geometry.location.lat);
+        // console.log(det.address_c omponents)
+      
+        
+        var number = det.address_components.filter(x => x.types.includes('street_number'))[0]
+        var street = det.address_components.filter(x => x.types.includes('route'))[0]
+        var city = det.address_components.filter(x => x.types.includes('locality'))[0]
+        var county = det.address_components.filter(x => x.types.includes('administrative_area_level_2'))[0]
+        var state = det.address_components.filter(x => x.types.includes('administrative_area_level_1'))[0]
+        var country = det.address_components.filter(x => x.types.includes('country'))[0]
+        var zip = det.address_components.filter(x => x.types.includes('postal_code'))[0]
+      
+        if(number && street && city && county && state){
+          // console.log(number)
+          // console.log(street)
+          // console.log(city)
+          // console.log(county)
+          // console.log(state)
+          // console.log(country)
+          // console.log(zip)
+          this.setState({
+            searchedAddress: true,
+            addressError: "",
+            address:{
+              ...this.state.address,
+              line1: `${number.long_name} ${street.short_name}`,
+              zipCode: zip.short_name,
+              city: city.long_name,
+              state: state.short_name,
+              country: country.short_name
+            }
+          })
+      
+          
+         
+        }else{
+          this.setState({addressError: "Select a valid street address"})
+          this.clearAddress();
+        }
+      
+      
+      
+        
+      }
+      
+      clearAddress = () => {
+        this.GooglePlacesRef.setAddressText("")
+        this.setState({
+          searchedAddress: false,
+          address: {
+            ...this.state.address,
+            line1: "",
+            zipCode: "",
+            city: "",
+            state: "",
+            country: ""
+          }
+        })
+      }
+      
+      setLocation = (text) => {
+        this.GooglePlacesRef && this.GooglePlacesRef.setAddressText(text)
+        // console.log("Set location")
+        console.log(this.state.address)
+      }
 
     updateProfile = () => {
         const db = firestore();
@@ -468,6 +611,12 @@ class Profile extends Component{
                             throw error
                 }
             }
+            // Check if address is updated
+            if(this.props.UserStore.address.line1 !== this.state.address.line1 || this.props.UserStore.address.city !== this.state.address.city || this.props.UserStore.address.state !== this.state.address.state || this.props.UserStore.address.postal_code !== this.state.address.zipCode){
+                this.addAddress()
+                this.setState({fullnameError: "", submitted: true}) 
+                setTimeout(() => this.setState({submitted: false}), 3000)
+            }
             if (this.state.fullNameUpdate != this.props.UserStore.fullname){
                 let error = null;
                 if (this.state.fullNameUpdate.match(regexFullname)){
@@ -689,7 +838,7 @@ class Profile extends Component{
                     
                     <SafeAreaView style={{paddingTop: 10, marginHorizontal: 16, flex: 1}}>
                                            
-                    <ScrollView>
+                    <ScrollView keyboardShouldPersistTaps="always">
                         <TopBar>
                         <Text style={{fontSize: 20, marginRight: 'auto', marginTop: 8, marginLeft: 16}}>Edit Profile</Text>
                             <Icon 
@@ -811,6 +960,150 @@ class Profile extends Component{
                             keyboardType='phone-pad'
                             error={this.state.dobError}
                         />
+                        <Text style={{fontSize: 15}}>Address</Text>
+                        <GooglePlacesAutocomplete
+                            placeholder='Your Address...'
+                            returnKeyType={'search'}
+                            ref={(instance) => { this.GooglePlacesRef = instance }}
+                            currentLocation={false}
+                            minLength={2}
+                            autoFocus={false}
+                            listViewDisplayed={false}
+                            fetchDetails={true}
+                            onChangeText= {(text) => this.setLocation(text)}
+                            onPress={(data, details = null) => {
+                            this.onSelectAddress(details)
+                            }}
+                            textInputProps={{
+                            clearButtonMode: 'never'
+                            }}
+                            renderRightButton={() => 
+                            <Icon 
+                            iconName="x"
+                            iconColor={Colors.cosmos500}
+                            iconSize={24}
+                            onPress={() => this.clearAddress()}
+                            style={{marginTop: 8, display: this.state.searchedAddress || this.state.address.line1 !== "" ? "flex" : "none",}}
+                            />}
+                            query={{
+                            key: 'AIzaSyBa1s5i_DzraNU6Gw_iO-wwvG2jJGdnq8c',
+                            language: 'en'
+                            }}
+                            GooglePlacesSearchQuery={{
+                            rankby: 'distance',
+                            types: 'address',
+                            components: "country:us"
+                            }}
+                            // GooglePlacesDetailsQuery={{ fields: 'geometry', }}
+                            nearbyPlacesAPI={'GoogleReverseGeocoding'}
+                            debounce={200}
+                            predefinedPlacesAlwaysVisible={true}
+                            enablePoweredByContainer={false}
+                            
+                            
+                            styles={{
+                            container: {
+                                border: 'none',
+                                zIndex: 99999
+                            },
+                            textInputContainer: {
+                                width: '100%',
+                                display: 'flex',
+                                alignSelf: 'center',
+                                backgroundColor: "white",
+                                marginTop: -6,
+                                borderColor: '#eee',
+                                borderBottomWidth: 2,
+                                borderTopWidth: 0,
+                                backgroundColor: "none",
+                            },
+                            textInput: {
+                                paddingRight: 0,
+                                paddingLeft: 0,
+                                paddingBottom: 0,
+                                color: '#333',
+                                fontSize: 18,
+                                width: '100%',
+                            },
+                            description: {
+                                fontWeight: 'bold'
+                            },
+                            predefinedPlacesDescription: {
+                                color: '#1faadb'
+                            },
+                            listView:{
+                                backgroundColor: 'green',
+                                position: 'absolute',
+                                top: 40,
+                                width: Dimensions.get("window").width - 32,
+                                zIndex: 999999,
+                            },
+                            row: {
+                              backgroundColor: 'white',
+                              
+                            },
+                            
+                            }}
+                        />
+                            <Text style={styles.error}>{this.state.addressError}</Text>
+                        {/* </View> */}
+                        
+                        {/* <Input
+                            flex={1}
+                            placeholder='107'        
+                            label= "Apt # (optional)"
+                            name="Apartment number" 
+                            style={{marginRight: 16}}                
+                            onChangeText= {(number) => this.setState(prevState => ({
+                            address:{
+                                ...prevState.address,
+                                line2: number,
+                            }
+                            }))}
+                            value={this.state.address.line2}
+                            maxLength = {6}
+                            keyboardType='number-pad'
+                        /> */}
+                        <View style={{flex: 1, zIndex: -9, flexDirection: 'row'}}>
+                        <Dropdown
+                            flex={2}
+                            selectedValue = {this.state.address.line2Prefix}
+                            label="Line 2 (optional)"
+                            // error={this.state.error.make}
+                            style={{height: 32}}
+                            onValueChange = {(res) => Platform.OS == 'ios' ? this.setState(prevState => ({address: {...this.state.address, line2Prefix: res.baseValue || prevState.address.line2Prefix}})) : this.setState({address: {...this.state.address, line2Prefix: res || "Hello"}})}
+                        >
+                            {
+                            AddressTypes.map((x, i) => {
+                            if(Platform.OS === 'ios'){
+                                return(
+                                {key: i, label: x, baseValue: x}
+                                )
+                            }
+                            else{
+                                return(
+                                    <DropdownItem key={i} label={x} value={x}/>
+                                )
+                            }
+                            })
+                            }
+                        </Dropdown>
+                        <Input
+                        flex={2}
+                        placeholder='107'        
+                        label= ""
+                        name="Apartment number"   
+                        style={{marginTop: 1.5, marginLeft: 8}}
+                        onChangeText= {(number) => this.setState(prevState => ({
+                            address:{
+                            ...prevState.address,
+                            line2: number,
+                            }
+                        }))}
+                        value={this.state.address.line2}
+                        maxLength = {6}
+                        />
+                        </View>
                          <ClickableChip
                                 style={{marginTop: 20, paddingTop: 10, paddingBottom: 10,}}
                                 onPress={() => this.updateAccountInfo()}
@@ -1054,6 +1347,15 @@ const styles = StyleSheet.create({
         fontSize: 16,
         alignSelf: 'center',
         marginTop: 24
+      },
+      error: {
+        paddingTop: 0,
+        paddingBottom: 0,
+        color: 'red',
+        fontSize: 14,
+        fontWeight: '400',
+        width: 'auto',
+        zIndex: -99
       }
 })
 
