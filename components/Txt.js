0edb980cd9a7f2c0c9bbeafd04cb4b0c10c8def1
Christopher Frydryck <cfrydryck@gmail.com>
2022-02-10 20:19:23 -0700

    Got android working with new npm commands

--- components/Txt.js
+++ components/Txt.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Text, StyleSheet, ActivityIndicator} from 'react-native'
+import Colors from '../constants/Colors'
 
 class Txt extends React.Component{ 
 
     render(){
         let lineHeight = this.props.style && this.props.style.fontSize ? this.props.style.fontSize + this.props.style.fontSize * .5 : 24;
 
-        const style = [{ lineHeight: lineHeight, fontSize: 16,fontFamily: this.props.type ? `Poppins-${this.props.type}` : 'Poppins-Regular'}, this.props.style || {}]
+        const style = [{ lineHeight: lineHeight, fontSize: 16, color: Colors.cosmos900,fontFamily: this.props.type ? `Poppins-${this.props.type}` : 'Poppins-Regular'}, this.props.style || {}]
         const allProps = Object.assign({}, this.props,{style:style})
 
             return(<Text {...allProps}>{this.props.children}</Text>)
